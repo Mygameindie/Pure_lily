339e54d6141ef8f5e5a43cfac899a7de075d0910
Mygameindie <poomkhrit14@gmail.com>
2026-01-12 14:08:33 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,53 @@
+// config.js
+
+window.jsonFiles = [
+  'bottomunderwear1.json','bottomunderwear2.json',
+  'topunderwear1.json','topunderwear2.json','stocking1.json',
+  'onepiece1.json',
+  'socks1.json','socks2.json',
+  'boxers1.json','boxers2.json',
+  'sweatshirt1.json','sweatshirt2.json',
+  'shoes1.json','shoes2.json',
+  'pants1.json','pants2.json',
+  'skirt1.json','skirt2.json',
+  'top1.json','top2.json',
+  'dress1.json','dress2.json',
+  // FIX: was ... 'jacket2.json',,'dress1w.json'
+  'jacket1.json','jacket2.json','dress1w.json',
+  'bunnysuitbow1.json','bunnysuitbow2.json',
+  'dress2w.json','skirt1w.json','skirt2w.json',
+  'accessories1.json','accessories2.json',
+  'hat1.json','hat2.json',
+  'mask1.json','mask2.json',
+  'bow1.json','bow2.json',
+];
+
+window.colorPalette = [
+  { name:'Original', value:'none' },
+  { name:'Red', value:'hue-rotate(0deg)' },
+  { name:'Blue', value:'hue-rotate(240deg)' },
+  { name:'Green', value:'hue-rotate(120deg)' },
+  { name:'Purple', value:'hue-rotate(270deg)' },
+  { name:'Orange', value:'hue-rotate(30deg)' },
+  { name:'Pink', value:'hue-rotate(320deg)' },
+  { name:'Yellow', value:'hue-rotate(60deg)' },
+  { name:'Cyan', value:'hue-rotate(180deg)' }
+];
+
+window.NAMED_HUES = {
+  Original: null,
+  Red: 0, Orange: 30, Yellow: 60, Green: 120,
+  Cyan: 180, Blue: 240, Purple: 270, Pink: 320
+};
+
+window.SWATCH_HEX = {
+  Original: null,
+  Red: '#ff3b30',
+  Orange: '#ff9500',
+  Yellow: '#ffcc00',
+  Green: '#34c759',
+  Cyan: '#32ade6',
+  Blue: '#007aff',
+  Purple: '#af52de',
+  Pink: '#ff2d55'
+};
